a477004a2670913904fbae4d893fc986e915a498
Nakamura Kazuki <nakahazu10@gmail.com>
2018-11-23 16:55:31 +0000

    認証の実装とテスト

--- app.js
+++ app.js
@@ -3,11 +3,40 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
+var session = require('express-session');
+var passport = require('passport');
+
+var GitHubStrategy = require('passport-github2').Strategy;
+var GITHUB_CLIENT_ID = '92bd150975735732ee89';
+var GITHUB_CLIENT_SECRET = '305fca705a7deaffbeea3463772f75361acdb6fc';
+
+passport.serializeUser(function(user,done) {
+  done(null,user);
+});
+
+passport.deserializeUser(function(obj,done) {
+  done(null,obj);
+});
+
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:8000/auth/github/callback'
+},
+  function(accessToken,refreshToken,profile,done) {
+    process.nextTick(function() {
+      return done(null,profile);
+    });
+  }
+));
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var logoutRouter = require('./routes/logout');
 
 var app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +48,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//githubのoauth設定と初期化
+app.use(session({ secret: '1e99c8d25ea9ae25' , resave: false , saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//ルーティング
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+
+//github認証の処理 (scopeの部分がよくわからん)
+app.get('/auth/github', 
+  passport.authenticate('github', { scope: ['user:email'] }),
+  function(req,res) {}
+);
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req,res) {
+    res.redirect('/');
+  }
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
